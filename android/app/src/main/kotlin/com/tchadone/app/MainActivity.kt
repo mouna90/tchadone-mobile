585c6345a7bbd84b1ac48d7332ced94ae91febe8
Mouna YACOUB <mounayacoubsaleh@yahoo.fr>
2023-04-28 11:25:23 +0200

    premier
    commit

---
+++ android/app/src/main/kotlin/com/tchadone/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tchadone.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/config.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Config {
+
+  
+  static const appName = 'Tchad One';
+  static const supportEmail = 'onemag@tchadone.com';
+  static const privacyPolicyUrl = 'https://tchadone.com/politique-de-confidentialite';
+  static const iOSAppID = '000000';
+
+
+
+  //SOCIAL LINKS
+  static const String facebookPageUrl = 'https://www.facebook.com/tchadone';
+  static const String youtubeChannelUrl = 'https://www.youtube.com/channel/tchadone_media';
+  static const String twitterUrl = 'https://twitter.com/tchadone';
+  
+
+
+  //app theme color
+  final Color appThemeColor = Color(0x00097a);
+
+
+  
+  // Icons
+  static const String appIcon = 'assets/images/icon.png';
+  static const String logo = 'assets/images/logo.png';
+  static const String splash = 'assets/images/splash.png';
+
+  
+
+  //languages
+  static const List<String> languages = [
+    'English',
+    'Spanish',
+    'Arabic'
+  ];
+
+
+
+  //Image Assets
+  static const String commentImage = "assets/images/comment.svg";
+  static const String bookmarkImage = "assets/images/bookmark.svg";
+  static const String notificationImage = "assets/images/notification.svg";
+  static const String noContentImage = "assets/images/no_content.svg";
+
+  
+  
+  //animation files
+  static const String doneAnimation = 'assets/animation_files/done.flr';
+  static const String searchAnimation = 'assets/animation_files/search.flr';
+
+  
+}
